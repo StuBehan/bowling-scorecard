b49d94dddad880924c92213b7e2522c548fad667
StuBehan <33100049+StuBehan@users.noreply.github.com>
2021-04-02 22:14:23 +0100

    ok lets start that again

--- lib/game.rb
+++ lib/game.rb
@@ -8,56 +8,6 @@ class Game
     @frames_total = []
   end
 
-  def bowl(bowl_one, bowl_two = 0, bowl_three = 0)
-    @frames << [bowl_one, bowl_two, bowl_three]
-    update_frames_total
-    find_total
-  end
 
-  def update_frames_total
-    if @frames.count == 10
-      @frames_total << tenth_frame
-    else
-      prev_id = 0
-      prev_prev_id = -1
-      @frames_total = []
-      @frames.each do |frame|
-        if frame[0] == 10  # strike
-          if @frames[prev_id][0] == 10
-            if @frames[prev_prev_id][0] == 10
-              @frames_total << 30
-            else
 
-            end
-
-          else
-
-          end
-        elsif frame[0] + frame[1] == 10 # spare
-          ft = 0
-        else # open frame
-          @frames_total << frame[0] + frame[1]
-        end
-        prev_id += 1
-        prev_prev_id += 1
-      end
-    end
-  end
-
-  def tenth_frame
-    ften = 0 
-    if @frames[9][0] == 10 || @frames[9][0] + @frames[9][1] == 10
-      @frames[9].each { |bowl| ften += bowl }
-    elsif ften = @frames[9][0] + @frames[9][1] 
-    end
-    ften
-  end
-
-  def find_total
-    @total = 0
-    
-    @frames_total.each do |frame_total|
-      @total += frame_total
-    end
-  end
 end
